149cf2b0c6eb290da4129de6fdb2337d7379a1c6
einn-pixel <haumutyeni@gmail.com>
2026-03-12 06:55:53 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -11,33 +11,20 @@ let kota = input.value.trim().toLowerCase()
 if(kota === "seattle"){
 
 fetch("/api/seattle")
-
 .then(response => response.json())
-
 .then(data => {
 
 result.innerHTML = `
-
 <h2>${data.nama_kota}</h2>
-
-<p><b>Negara:</b> ${data.negara}</p>
-
-<p><b>Negara Bagian:</b> ${data.negara_bagian}</p>
-
-<p><b>Luas Kota:</b> ${data.luas_kota}</p>
-
-<p><b>Jumlah Penduduk:</b> ${data.jumlah_penduduk}</p>
-
-<p><b>Tahun Didirikan:</b> ${data.tahun_didirikan}</p>
-
-<p><b>Julukan Kota:</b> ${data.julukan}</p>
-
-<p><b>Iklim:</b> ${data.iklim}</p>
-
-<p><b>Tempat Wisata:</b> ${data.tempat_wisata}</p>
-
-<p><b>Universitas:</b> ${data.universitas}</p>
-
+<p>Negara: ${data.negara}</p>
+<p>Negara Bagian: ${data.negara_bagian}</p>
+<p>Luas Kota: ${data.luas_kota}</p>
+<p>Jumlah Penduduk: ${data.jumlah_penduduk}</p>
+<p>Tahun Didirikan: ${data.tahun_didirikan}</p>
+<p>Julukan Kota: ${data.julukan}</p>
+<p>Iklim: ${data.iklim}</p>
+<p>Tempat Wisata: ${data.tempat_wisata}</p>
+<p>Universitas: ${data.universitas}</p>
 `
 
 })
